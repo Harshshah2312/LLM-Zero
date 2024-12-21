5174a6a10d5e633bdafe8157d56a8d8714d018f1
Siddharth Kale <siddharthvaidon@gmail.com>
2024-12-21 08:05:01 +0000

    chore: Initial commit

---
+++ app/run.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:$LAMBDA_TASK_ROOT/bin
+export PYTHONPATH=/opt/python:$LAMBDA_RUNTIME_DIR
+LOG_LEVEL=${AWS_LAMBDA_LOG_LEVEL:-info}
+PORT=${PORT:-8080}
+exec python -m uvicorn --workers=1 --log-level=$LOG_LEVEL --port=$PORT main:app
